976e45d03400dc1a47d955c499fca90c067648c2
Talaxy009 <x009ba1@gmail.com>
2022-11-10 00:07:01 +0800

    ⚡️ Optimize task management and caching logic

--- src/linkPreview.js
+++ src/linkPreview.js
@@ -1,71 +1,47 @@
 const puppeteer = require('puppeteer');
 const {fluid} = require('gatsby-plugin-sharp');
-const EventEmitter = require('events').EventEmitter;
-
-const {buildImg} = require('./utils');
-
-const emitter = new EventEmitter();
 
 /**
  * get page data from url by puppeteer
  * @param {puppeteer.Page} page puppeteer page object
+ * @param {object} file file obj
  * @param {string} url the url to be fetched
  * @param {object} options options
  * @returns image file obj
  */
-const getPageScreenshot = async (page, data, options) => {
+const getPageScreenshot = async (page, file, data, options) => {
     const {url, reporter} = data;
-    const file = buildImg(url);
-
-    // Check if a screenshot with the same name has already been done
-    if (global.LINK_BEAUTIFY_PVIMG_FINISHED.has(file.name)) {
-        return file;
-    }
-    // Check if a screenshot with the same name is being processed
-    if (global.LINK_BEAUTIFY_PVIMG_PROCESSING.has(file.name)) {
-        return new Promise((resolve) => {
-            emitter.once(`linkBeautifyDone-${file.name}`, () => {
-                resolve(file);
-            });
-        });
-    }
-    global.LINK_BEAUTIFY_PVIMG_PROCESSING.add(file.name);
 
     try {
         await page.goto(url, {timeout: options.timeout, waitUntil: 'load'});
         await page.screenshot({path: file.absolutePath});
 
-        // Move this screenshot to the FINISHED set and emit the event
-        global.LINK_BEAUTIFY_PVIMG_FINISHED.add(file.name);
-        global.LINK_BEAUTIFY_PVIMG_PROCESSING.delete(file.name);
-        emitter.emit(`linkBeautifyDone-${file.name}`);
-
-        return file;
+        return true;
     } catch (e) {
         reporter.warn(`link-beautify: Unable to get screenshot from ${url}`);
-        return null;
+        return false;
     }
 };
 
 /**
  * build html string from page data
  * @param {object} node link node
- * @param {string} file image path
+ * @param {object} screenshot screenshot obj
  * @param {number} quality image quality
  * @returns html string
  */
-const getHTML = async (data, file, quality) => {
+const getHTML = async (data, screenshot, quality) => {
     const {node, reporter, cache} = data;
     const linkHtml = `
     <a target="_blank" rel="noopener noreferrer" href="${node.url}">
         ${node.children[0].value}
     </a>`;
 
-    if (!file) return linkHtml;
+    if (!screenshot.success) return linkHtml;
 
     try {
         const fluidResult = await fluid({
-            file,
+            file: screenshot.file,
             args: {maxWidth: 800, srcSetBreakpoints: [800], quality},
             reporter,
             cache,
--- src/taskManagement.js
+++ src/taskManagement.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const md5 = require('md5');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const EventEmitter = require('events').EventEmitter;
@@ -6,7 +7,7 @@ const EventEmitter = require('events').EventEmitter;
 const {getHTML: getCardHTML, getPageData} = require('./linkCard');
 const {getHTML: getPreviewHTML, getPageScreenshot} = require('./linkPreview');
 
-const {isLinkCard} = require('./utils');
+const {isLinkCard, buildImg} = require('./utils');
 
 const emitter = new EventEmitter();
 const PAGE_NUMBER_PER_BROWSER = 5;
@@ -33,11 +34,11 @@ const init = async (options) => {
         }
     }
     global.WSE_LIST = [];
-    global.PUPPETEER_PAGE_NUMBER = 0;  // Current page count
+    global.PUPPETEER_PAGE_NUMBER = 0; // Current page count
     global.LINK_BEAUTIFY_LISTENER = 0; // Listener number
-    global.LINK_BEAUTIFY_CALLER = 0; // Emitter number
-    global.LINK_BEAUTIFY_PVIMG_PROCESSING = new Set(); // Processing images set
-    global.LINK_BEAUTIFY_PVIMG_FINISHED = new Set(); // Finished images set
+    global.LINK_BEAUTIFY_TASKS = []; // Waitting tasks array
+    global.LINK_BEAUTIFY_IMG = new Map(); // Images' map
+    global.LINK_BEAUTIFY_CARD = new Map(); // Cards' map
     while (WSE_LIST.length < num) {
         const browser = await puppeteer.launch({args});
         WSE_LIST.push(browser.wsEndpoint());
@@ -52,7 +53,6 @@ const close = async () => {
     // If there are no more taskgroups running, reset global variables
     if (PUPPETEER_PAGE_NUMBER === 0) {
         LINK_BEAUTIFY_LISTENER = 0;
-        LINK_BEAUTIFY_CALLER = 0;
         // Do not close the browser if in development mode
         if (process.env.NODE_ENV !== 'development') {
             while (WSE_LIST.length) {
@@ -78,8 +78,10 @@ const free = (tasksNum) => {
         return;
     }
     // Other taskgroups will wait for free tabs
+    const number = ++LINK_BEAUTIFY_LISTENER;
+    LINK_BEAUTIFY_TASKS.push({no: number, size: tasksNum});
     return new Promise((resolve) => {
-        emitter.once(`linkBeautifyFree-${++LINK_BEAUTIFY_LISTENER}`, resolve);
+        emitter.once(`linkBeautifyFree-${number}`, resolve);
     });
 };
 
@@ -98,11 +100,11 @@ const newPage = (browser) => {
  */
 const closePage = (page) => {
     // If there are free spaces && there are taskgroups waitting, run the next task
-    if (
-        PAGE_NUMBER_PER_BROWSER * WSE_LIST.length > --PUPPETEER_PAGE_NUMBER &&
-        LINK_BEAUTIFY_LISTENER > LINK_BEAUTIFY_CALLER
-    ) {
-        emitter.emit(`linkBeautifyFree-${++LINK_BEAUTIFY_CALLER}`);
+    let freeNum = PAGE_NUMBER_PER_BROWSER * WSE_LIST.length - --PUPPETEER_PAGE_NUMBER;
+    while (freeNum > 0 && LINK_BEAUTIFY_TASKS.length > 0) {
+        const task = LINK_BEAUTIFY_TASKS.shift();
+        emitter.emit(`linkBeautifyFree-${task.no}`);
+        freeNum -= task.size;
     }
     return page.close();
 };
@@ -117,26 +119,65 @@ const task = async (data, options) => {
         WSE_LIST[Math.floor(Math.random() * WSE_LIST.length)];
     const browser = await puppeteer.connect({browserWSEndpoint});
 
-    const {node, url, cache} = data;
+    const {node, url} = data;
+    const name = md5(url);
     let html;
 
     if (isLinkCard(node, options.delimiter)) {
-        // If there are data in cache, return it
-        html = await cache.get(`linkCard-${url}`);
-        if (!html) {
+        let meta = null;
+
+        // Check if a site with the same url is being processed
+        if (!LINK_BEAUTIFY_CARD.has(name)) {
+            LINK_BEAUTIFY_CARD.set(name, null);
             const page = await newPage(browser);
-            const meta = await getPageData(page, data, options);
-            html = getCardHTML(meta, options.showFavicon);
-            if (meta.success) {
-                await cache.set(`linkCard-${url}`, html);
-            }
+            meta = await getPageData(page, data, options);
             await closePage(page);
+
+            // Store meta and emit the event
+            LINK_BEAUTIFY_CARD.set(name, meta);
+            emitter.emit(`linkCardDone-${name}`);
+        // Check if a site with the same url has already been done
+        } else if (!LINK_BEAUTIFY_CARD.get(name)) {
+            meta = await new Promise((resolve) => {
+                emitter.once(`linkCardDone-${name}`, () => {
+                        resolve(LINK_BEAUTIFY_CARD.get(name));
+                });
+            });
+        } else {
+            meta = LINK_BEAUTIFY_CARD.get(name);
         }
+
+        html = getCardHTML(meta, options.showFavicon);
     } else {
-        // Do not use cache object as images are stored in cache files
-        const page = await newPage(browser);
-        const screenshot = await getPageScreenshot(page, data, options);
-        await closePage(page);
+        const file = buildImg(url);
+        const screenshot = {
+            success: true, // Whether the screenshot was successfully captured and saved
+            file,
+        }
+
+        // Check if a screenshot with the same name is being processed
+        if (!LINK_BEAUTIFY_IMG.has(name)) {
+            LINK_BEAUTIFY_IMG.set(name, false);
+            const page = await newPage(browser);
+            screenshot.success = await getPageScreenshot(page, file, data, options);
+            await closePage(page);
+
+            // Set screenshot status and emit the event
+            if (screenshot.success) {
+                LINK_BEAUTIFY_IMG.set(name, true);
+            } else {
+                LINK_BEAUTIFY_IMG.delete(name);
+            }
+            emitter.emit(`linkPreviewDone-${name}`);
+        // Check if a screenshot with the same name has already been done
+        } else if (!LINK_BEAUTIFY_IMG.get(name)) {
+            screenshot.success = await new Promise((resolve) => {
+                emitter.once(`linkPreviewDone-${name}`, () => {
+                    resolve(Boolean(LINK_BEAUTIFY_IMG.get(name)));
+                });
+            });
+        }
+
         html = await getPreviewHTML(
             data,
             screenshot,
